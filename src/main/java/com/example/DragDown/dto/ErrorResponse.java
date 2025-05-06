5ea4ac194d529a1b9830bc886afe4c1d6d688e6a
Coffeecaat <juniya070894@gmail.com>
2025-05-06 17:42:30 +0900

    Feat: 에러 메시지 DTO, 정상 응답 메시지 DTO 추가

---
+++ src/main/java/com/example/DragDown/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.DragDown.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String error;
+    private String message;
+}
