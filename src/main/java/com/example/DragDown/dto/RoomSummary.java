e9051fd9ce857aec0e6045f477115543c2247224
Coffeecaat <coffeecat@DESKTOP-NMDDRKI>
2025-05-06 18:03:32 +0900

    Feat: Matchrooms 상세 정보 DTO, 요약 정보 DTO 추가

---
+++ src/main/java/com/example/DragDown/dto/RoomSummary.java
@@ -0,0 +1,18 @@
+package com.example.DragDown.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomSummary {
+
+    private String roomId;
+    private String roomName;
+    private String hostUsername;
+    private int currentPlayerCount;
+    private int maxPlayers;
+    private boolean GameStarted;
+}
